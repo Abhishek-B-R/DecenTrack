65a228b57075824143411ecedc189f382eb8382f
Abhishek-B-R <byteblaster1468@gmail.com>
2025-07-28 21:53:12 +0530

    added a mention that this project is built over ethereum

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -13,7 +13,7 @@ export default function LandingPage() {
               <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2">
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                    Decentralized Website Uptime Monitoring
+                    Decentralized Website Uptime Monitoring built entirely on Ethereum
                   </h1>
                   <p className="max-w-[600px] text-muted-foreground md:text-xl">
                     Monitor website uptime with the power of blockchain. Add your sites or validate others and earn
